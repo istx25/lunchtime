6a9db026d0ac662e432eacf598eaa8286e93371d
Douglas Bumby <douglas.bumby@icloud.com>
2015-11-13 20:21:28 -0800

    Add setup flow UI

---
+++ Lunchtime/Lunchtime/Controller/SetupPageSecondViewController.h
@@ -0,0 +1,12 @@
+//
+//  SetupPageSecondViewController.h
+//  Lunchtime
+//
+//  Created by Willow Belle on 2015-11-13.
+//  Copyright © 2015 Cosmic Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetupPageSecondViewController : UIViewController
+@end
---
+++ Lunchtime/Lunchtime/Controller/SetupPageThirdViewController.h
@@ -0,0 +1,12 @@
+//
+//  SetupPageThirdViewController.h
+//  Lunchtime
+//
+//  Created by Willow Belle on 2015-11-13.
+//  Copyright © 2015 Cosmic Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetupPageThirdViewController : UIViewController
+@end
