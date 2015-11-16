a7dc10a6070fa34d1f0003d388aa2f0629f4a319
Luna Graysen <luna.graysen@gmail.com>
2015-11-16 12:20:33 -0800

    Created the new class and table view cell

---
+++ Lunchtime/View/Helper/UIColor+Lunchtime.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+Lunchtime.h
+//  Lunchtime
+//
+//  Created by Willow Belle on 2015-11-16.
+//  Copyright © 2015 Cosmic Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Lunchtime)
+
++ (UIColor *)lightGray;
+
+@end
