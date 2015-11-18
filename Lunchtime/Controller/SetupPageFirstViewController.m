24de8484ea31ccc5550660198c9b91f495a11437
Douglas Bumby <douglas.bumby@icloud.com>
2015-11-17 20:47:17 -0800

    Implemented erase methods

---
+++ Lunchtime/Controller/SetupPageFirstViewController.m
@@ -0,0 +1,19 @@
+//
+//  SetupPageFirstViewController.m
+//  Lunchtime
+//
+//  Created by Willow Belle on 2015-11-17.
+//  Copyright © 2015 Cosmic Labs. All rights reserved.
+//
+
+#import "SetupPageFirstViewController.h"
+
+@implementation SetupPageFirstViewController
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+
+    [self.navigationController setNavigationBarHidden:YES animated:NO];
+}
+
+@end
