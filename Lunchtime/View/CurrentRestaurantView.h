393ed726c6351446a6e433f2f1f3bdefa07f12cf
Douglas Bumby <douglas.bumby@icloud.com>
2015-11-18 14:31:24 -0800

    Commit constraints

---
+++ Lunchtime/View/CurrentRestaurantView.h
@@ -0,0 +1,12 @@
+//
+//  CurrentRestaurantView.h
+//  Lunchtime
+//
+//  Created by Willow Belle on 2015-11-18.
+//  Copyright © 2015 Cosmic Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CurrentRestaurantView : UIView
+@end
