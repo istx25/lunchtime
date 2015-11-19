c6fbe18be11fa6ccdfb0ebae8a77cbb1accabde1
Douglas Bumby <douglas.bumby@icloud.com>
2015-11-18 17:35:09 -0800

    Rewrite primary page view controller in Swift

---
+++ Lunchtime/Lunchtime-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Lunchtime-Bridging-Header.h
+//  Lunchtime
+//
+//  Created by Willow Belle on 2015-11-18.
+//  Copyright © 2015 Cosmic Labs. All rights reserved.
+//
+
+#import "LunchtimeLocationManager.h"
+
+#import "SetupPageFirstViewController.h"
+#import "SetupPageSecondViewController.h"
+#import "SetupPageThirdViewController.h"
+#import "SetupPageFourthViewController.h"
+#import "SetupPageFifthViewController.h"
+#import "SetupPageSixthViewController.h"
